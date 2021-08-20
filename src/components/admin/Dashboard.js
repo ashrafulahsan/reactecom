85fe351d017b0cf807cf0e5c38405641c6fb8a37
ashrafulahsan <ashrafulahsan@gmail.com>
2021-08-21 00:11:36 +0600

    HTML to React js admin panel

---
+++ src/components/admin/Dashboard.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {Link} from 'react-router-dom';
+
+const Dashboard = () => {
+    return (
+        <h1>Dashboard</h1>
+    );
+}
+
+export default Dashboard;
---
+++ src/components/admin/Profile.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {Link} from 'react-router-dom';
+
+const Profile = () => {
+    return (
+        <h1>Profile</h1>
+    );
+}
+
+export default Profile;
